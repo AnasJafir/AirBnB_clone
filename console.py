0856a855fa4fda4ca89f33093345bb8a284d3d67
AnasJafir <dr.anas.jafir@gmail.com>
2023-11-14 14:25:09 -0800

    More classes

--- console.py
+++ console.py
@@ -7,7 +7,11 @@ import shlex
 from models.base_model import BaseModel
 from models import storage
 from models.user import User
-
+from models.review import Review
+from models.state import State
+from models.city import City
+from models.place import Place
+from models.amenity import Amenity
 
 class HBNBCommand(cmd.Cmd):
     """
---
+++ models/city.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+City Module
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """
+    City class to handle informations
+    """
+    state_id = ""
+    name = ""
