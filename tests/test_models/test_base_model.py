d2336ee0e551db4a267a310dadd46c9344c8012c
AnasJafir <dr.anas.jafir@gmail.com>
2023-11-14 09:02:10 -0800

    Creating test file for base model module

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+"""
+unit tests for the functions in base_model.py
+"""
+import unittest
+from models.base_model import BaseModel
+
+class TestBaseModel(unittest.TestCase):
+    def test_init(self):
+        _model = BaseModel()
+        self.assertIsNotNone(_model.id)
+        self.assertIsNotNone(_model.created_at)
+        self.assertIsNotNone(_model.updated_at)
+    def test_sqve(self):
+        _model = BaseModel()
+        prev_updated_at = _model.updated_at
+        curr_updated_at = _model.save()
+        self.assertNotEqual(prev_updated_at, curr_updated_at)
+    def test_to_dict(self):
+        _model = BaseModel()
+        _model_dict = _model.to_dict()
+        self.assertIsInstance(_model_dict, dict)
+        self.assertEqual(_model_dict["__class__"], 'BaseModel')
+        self.assertEqual(_model_dict['id'], _model.id)
+        self.assertEqual(_model_dict['created_at'], _model.created_at.isoformat())
+        self.assertEqual(_model_dict['updated_at'], _model.updated_at.isoformat())
+    def test_str(self):
+        _model = BaseModel()
+        self.assertTrue(str(_model).startswith('[BaseModel]'))
+        self.assertIn(_model.id, str(_model))
+        self.assertIn(str(_model.__dict__), str(_model))
+if __name__ == '__main__':
+    unittest.main()
