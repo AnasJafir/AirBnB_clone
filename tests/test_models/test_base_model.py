122297003e6629b2c5cf36214fc212145fe3b3d0
AnasJafir <dr.anas.jafir@gmail.com>
2023-11-14 10:27:11 -0800

    Correcting requirements issues

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
 """
-unit tests for the functions in base_model.py
+Module for BaseModel unittest
 """
 import unittest
 from models.base_model import BaseModel
 
 
-class TestBaseModel(unittest.TestCase):
+class TestBasemodel(unittest.TestCase):
     """
-    BaseModel Unittest
+    Unittest for BaseModel
     """
 
     def test_init(self):
         """
-        Objects initialization test case
+        Test for init
         """
         _model = BaseModel()
+
         self.assertIsNotNone(_model.id)
         self.assertIsNotNone(_model.created_at)
         self.assertIsNotNone(_model.updated_at)
@@ -26,15 +27,15 @@ class TestBaseModel(unittest.TestCase):
         """
         _model = BaseModel()
 
-        initial_updated_at = _model.updated_at
+        init_updated_at = _model.updated_at
 
-        current_updated_at = _model.save()
+        curr_updated_at = _model.save()
 
-        self.assertNotEqual(initial_updated_at, current_updated_at)
+        self.assertNotEqual(init_updated_at, curr_updated_at)
 
     def test_to_dict(self):
         """
-        To dictionnary method test case
+        To dictionary method test case
         """
         _mdl = BaseModel()
         _mdl_dict = _mdl.to_dict()
@@ -42,17 +43,20 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(_mdl_dict["__class__"], 'BaseModel')
         self.assertEqual(_mdl_dict['id'], _mdl.id)
         self.assertEqual(_mdl_dict['created_at'], _mdl.created_at.isoformat())
-        self.assertEqual(_mdl_dict['updated_at'], _mdl.updated_at.isoformat())
+        self.assertTrue(_mdl_dict['updated_at'].startswith(_mdl.updated_at.isoformat()))
 
     def test_str(self):
         """
-        str method test case
+        Test for string representation
         """
         _model = BaseModel()
+
         self.assertTrue(str(_model).startswith('[BaseModel]'))
+
         self.assertIn(_model.id, str(_model))
+
         self.assertIn(str(_model.__dict__), str(_model))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
