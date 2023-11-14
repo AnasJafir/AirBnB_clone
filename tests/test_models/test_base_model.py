eb34fea520f3ab0760e073cf12368295326f0c88
AnasJafir <dr.anas.jafir@gmail.com>
2023-11-14 09:14:53 -0800

    Correcting documentation issues

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -7,16 +7,25 @@ from models.base_model import BaseModel
 
 class TestBaseModel(unittest.TestCase):
     def test_init(self):
+        """
+        Objects initialization test case
+        """
         _model = BaseModel()
         self.assertIsNotNone(_model.id)
         self.assertIsNotNone(_model.created_at)
         self.assertIsNotNone(_model.updated_at)
     def test_save(self):
+        """
+        Save method test case
+        """
         _model = BaseModel()
         prev_updated_at = _model.updated_at
         curr_updated_at = _model.save()
         self.assertNotEqual(prev_updated_at, curr_updated_at)
     def test_to_dict(self):
+        """
+        To dictionnary method test case
+        """
         _model = BaseModel()
         _model_dict = _model.to_dict()
         self.assertIsInstance(_model_dict, dict)
@@ -25,6 +34,9 @@ class TestBaseModel(unittest.TestCase):
         self.assertEqual(_model_dict['created_at'], _model.created_at.isoformat())
         self.assertEqual(_model_dict['updated_at'], _model.updated_at.isoformat())
     def test_str(self):
+        """
+        str method test case
+        """
         _model = BaseModel()
         self.assertTrue(str(_model).startswith('[BaseModel]'))
         self.assertIn(_model.id, str(_model))
